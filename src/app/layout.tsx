b170762bd048d45abc0b761bf3cfc9790db41870
motidev <angelnabro@gmail.com>
2024-08-26 16:53:56 +0200

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "SnapCode",
+  description: "Test your code easily",
+};
+
+export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
