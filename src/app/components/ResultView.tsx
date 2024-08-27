7216229469d24a20220bbbfcd829050b53671c02
motidev <angelnabro@gmail.com>
2024-08-27 16:40:56 +0200

    Update style

--- src/app/components/ResultView.tsx
+++ src/app/components/ResultView.tsx
@@ -8,7 +8,7 @@ const ResultView: FC<ResultViewProps> = ({ output }) => {
     return (
         <div>
             <h1 className='font-bold'>Result</h1>
-            <pre className='font-normal text-sm text-gray-500'>{output}</pre>
+            <pre className='font-normal text-sm text-purple-200 opacity-80'>{output}</pre>
         </div>
     );
 }
