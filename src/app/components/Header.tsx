6becf2ab7fd8d66f036c5e5f4c2e437d07b5712b
Moti <angelnabro@gmail.com>
2024-08-30 00:12:53 +0200

    refactor(main): update main page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Header = () => {
     return (
-        <nav>
+        <nav> 
             <div className='flex items-center'>
                 <Image src="/snipcode.svg" width={100} height={100} alt=''></Image>
                 <h1 className='text-2xl font-extrabold text-white'>NIPCODE</h1>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 function HomePage() {
   return (
-    <div>
-      <p>hola</p>
+    <div className='ml-16'>
+      <div className='mt-2'>
+        <Link className="bg-red-900 text-red-500 bg-opacity-30 p-1 rounded-md text-xs" href='https://github.com/motidev/snipcode'>Snipcode Github 💭</Link>
+      </div>
+      <div>
+        <h1 className='font-extrabold select-none text-4xl mt-7'>Test your <span className='bg-red-900 text-red-700 bg-opacity-15 p-2 rounded-lg'>code</span> easily</h1>
+        <div className='text-sm mt-7 text-white text-opacity-50'>
+            <p>Snipcode is a cutting-edge web app for coding, learning, and experimenting.</p>
+            <p>Run and debug code instantly, all from your browser.</p>
+            <p>Supports multiple languages with no installations needed.</p>
+            <p>Plus, it's your personal library for storing and sharing code snippets.</p>
+        </div>
+
+
+
+      </div>
+
     </div>
   );
 }
 
-export default HomePage;
+export default HomePage
